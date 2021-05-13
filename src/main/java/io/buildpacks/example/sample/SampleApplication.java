388c80a33ec4019ed1b41bc130a8c412d3fbd583
QinhuaZhao <Qinhua_Zhao@outlook.com>
2021-05-13 21:49:05 +0800

    Update SampleApplication.java
    1

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApplication {
 
-	public static void main(String[] args) {\
+	public static void main(String[] args) {
 		System.out.println("hello buildpacks");
 		SpringApplication.run(SampleApplication.class, args);
 	}
