596730697011237897602c171db61902d009a4f3
QinhuaZhao <Qinhua_Zhao@outlook.com>
2021-05-14 09:04:37 +0800

    Update SampleApplication.java
    1

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -9,6 +9,7 @@ public class SampleApplication {
 	public static void main(String[] args) {
 		System.out.println("hello buildpacks");
 		System.out.println("hello kpack");
+		System.out.println("hello kpack");
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
